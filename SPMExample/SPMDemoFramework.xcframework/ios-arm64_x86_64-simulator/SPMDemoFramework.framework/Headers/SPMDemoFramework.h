1c5815e92e1bd9587e32165d03fe0243fbd63d16
reemabhalani <reema.bhalani@gmail.com>
2023-06-14 15:18:04 -0700

    Add xcframework

---
+++ SPMExample/SPMDemoFramework.xcframework/ios-arm64_x86_64-simulator/SPMDemoFramework.framework/Headers/SPMDemoFramework.h
@@ -0,0 +1,18 @@
+//
+//  SPMDemoFramework.h
+//  SPMDemoFramework
+//
+//  Created by Reema Bhalani on 6/9/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPMDemoFramework.
+FOUNDATION_EXPORT double SPMDemoFrameworkVersionNumber;
+
+//! Project version string for SPMDemoFramework.
+FOUNDATION_EXPORT const unsigned char SPMDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMDemoFramework/PublicHeader.h>
+
+
